9b48967897449b457b39b1b1e06f658f0206e51d
Aekangi Patel <aekangi91@gmail.com>
2023-02-05 20:28:43 -0500

    cleaned up the pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,35 +1,13 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
 
 import React from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/mosaic.svg";
 
 const imageAltText = "purple and blue abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
   "I am a software developer with an eye for details. My previous experience as a baker has taught me how to appreciate every component that goes into making a masterpiece — and how to do that while working collaboratively with others in fast-paced, deadline-driven environments. Coder by day and baker by night, I am excited to contribute my skills in team management, designing, and coding with my adaptability, creativity, and people skills to help companies solve problems and achieve their goals.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
   "JavaScript",
   "Python",
@@ -39,11 +17,6 @@ const skillsList = [
   "SQL",
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote = "Software is easy to make, except when you want it to do something new. And then, of course, there is a corollary: The only software that's worth making is software that does something new. Scott Rossenberg";
 const About = () => {
   return (
@@ -52,7 +25,7 @@ const About = () => {
       <div
         style={{
           backgroundColor: "rgb(202, 193, 223)",
-          // opacity: ".9",
+          opacity: ".9",
           width: "50%",
           padding: "4rem",
           margin: "3rem auto",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,24 +1,4 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/computercoding.jpeg";
 import image1 from "../images/womencoding.jpeg";
 import image2 from "../images/code.jpeg";
@@ -26,13 +6,6 @@ import image2 from "../images/code.jpeg";
 const imageAltText = "computer code";
 const image1women = "women on computer";
 const image2code = "coding image"
-
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "FitNow",
@@ -103,7 +76,7 @@ const Portfolio = () => {
                 <p className="small">{project.description3}</p>
                 <a href={project.gitHub} target="_blank" rel="noopener noreferrer">
                   <h3 style={{ flexBasis: "40px" }}>Github Repository</h3>
-                </a>
+                </a> 
               </div>
             ))}
           </div>
